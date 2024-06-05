677608913df67cf50308ae819e85f3e85873e799
ISRAEL_GUIJARRO <2230082@upv.edu.mx>
2024-06-04 22:53:06 -0600

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static final String url="https://www.imdb.com/name/nm0000115/";
+    public static void main( String[] args )
+    {
+        if(getStatusConnectionCode(url)==200){
+
+            Document dc= getHtmlDocument(url);
+            Elements entradas=dc.select("div");
+            System.out.println(entradas.size());
+            for(Element elem : entradas){
+                System.out.println(elem.getElementsByClass("movie").text());
+            }
+        }
+    }
+
+
+    public static int getStatusConnectionCode (String url){
+        Connection.Response respomse=null;
+        try{
+            respomse= Jsoup.connect(url).userAgent("Mozilla Firefox 126.0.1").timeout(100000).ignoreHttpErrors(true).execute();
+        }catch(IOException e){
+            System.out.println("Excepcion al obtener el Status COde: "+ e.getMessage());
+        }
+        return respomse.statusCode();
+    }
+
+    public static Document getHtmlDocument(String url){
+        Document doc=null;
+        try{
+            doc=Jsoup.connect(url).userAgent("Mozilla Firefox 126.0.1").timeout(100000).get();
+        }catch(IOException e){
+            System.out.println("Excepcion al obtener el Htlm de la página: "+ e.getMessage());
+        }
+        return doc;
+    }
+}
+
+
